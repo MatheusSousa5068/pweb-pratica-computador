bb28a0f4156e457772a089cade28458c6047e616
MatheusSousa5068 <matheus5068trab@gmail.com>
2023-10-02 23:28:18 -0300

    creating computer

---
+++ src/app/shared/models/computador.ts
@@ -0,0 +1,40 @@
+export class Computador {
+  constructor(
+    private _marca: string,
+    private _modelo: string,
+    private _processador: string,
+    private _ram: number
+  ) {}
+
+  get marca(): string {
+    return this._marca;
+  }
+
+  set marca(novaMarca: string) {
+    this._marca = novaMarca;
+  }
+
+  get modelo(): string {
+    return this._modelo;
+  }
+
+  set modelo(novoModelo: string) {
+    this._modelo = novoModelo;
+  }
+
+  get processador(): string {
+    return this._processador;
+  }
+
+  set processador(novoProcessador: string) {
+    this._processador = novoProcessador;
+  }
+
+  get ram(): number {
+    return this._ram;
+  }
+
+  set ram(novaRam: number) {
+    this._ram = novaRam;
+  }
+}
