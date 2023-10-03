831e86466bd4e9662f8f83699b77609992ee017b
MatheusSousa5068 <matheus5068trab@gmail.com>
2023-10-02 23:41:17 -0300

    creating app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,24 +4,53 @@ import { Computador } from './shared/models/computador';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'pweb-computador';
   computadores: Computador[] = [];
   computadorTratamento: Computador;
+  computadoresPesquisa: Computador[] = [];
+
+  mensagemErro = '';
 
   constructor() {
-    this.computadorTratamento = new Computador('', '', '', 0);
+    this.computadorTratamento = new Computador('', '', '', '', 0);
   }
 
   cadastrar(): void {
-    if (!this.ehMatriculaCadastrada(this.alunoTratamento.matricula)) {
-      this.alunos.push(this.alunoTratamento);
-      this.alunoTratamento = new Aluno('', '', 0);
+    if (!this.ehNumeroSerieCadastrado(this.computadorTratamento.numeroSerie)) {
+      this.computadores.push(this.computadorTratamento);
+      this.computadorTratamento = new Computador('', '', '', '', 0);
       this.mensagemErro = '';
     } else {
-      this.mensagemErro = `Matrícula ${this.alunoTratamento.matricula} já cadastrada!`;
+      this.mensagemErro = `Número de série ${this.computadorTratamento.numeroSerie} já cadastrado!`;
     }
+  }
+
+  private ehNumeroSerieCadastrado(numeroSerie: string): boolean {
+    return this.computadores.some(
+      (computador) => computador.numeroSerie === numeroSerie
+    );
+  }
+
+  remover(computadorARemover: Computador): void {
+    const indxARemover = this.computadores.findIndex(
+      (computador) => computador.numeroSerie === computadorARemover.numeroSerie
+    );
+
+    if (indxARemover >= 0) {
+      this.computadores.splice(indxARemover, 1);
+    }
+  }
+
+  pesquisar(pedacoNumeroSerie: string): void {
+    if (pedacoNumeroSerie.length === 0) {
+      this.computadoresPesquisa = [];
+    }
+
+    this.computadoresPesquisa = this.computadores.filter((computador) =>
+      computador.numeroSerie.includes(pedacoNumeroSerie)
+    );
   }
 }
